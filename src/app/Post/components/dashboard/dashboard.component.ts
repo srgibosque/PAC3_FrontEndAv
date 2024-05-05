3662f589da11cf2dfa0ac1ca74db0f4db8d682ff
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-05-05 19:24:01 +0200

    bar chart for dashboard

--- src/app/Post/components/dashboard/dashboard.component.ts
+++ src/app/Post/components/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
 import * as PostsAction from '../../actions';
 import { PostDTO } from '../../models/post.dto';
+import { Chart, ChartType } from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,11 +14,46 @@ export class DashboardComponent implements OnInit {
   posts: PostDTO[];
   numLikes: number;
   numDislikes: number;
+  chart!: Chart
 
   constructor(private store: Store<AppState>) {
     this.posts = new Array<PostDTO>();
     this.numLikes = 0;
     this.numDislikes = 0;
+  }
+
+  ngOnInit(): void {
+    this.loadPosts();
+    const data = {
+      labels: ["Likes", "Dislikes"],
+      datasets: [{
+        axis: 'y',
+        label: 'Number of likes',
+        data: [0, 0],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.5)',
+          'rgba(54, 162, 235, 0.5)',
+        ],
+        borderColor: [
+          'rgb(54, 162, 235)',
+          'rgb(54, 162, 235)',
+        ],
+        borderWidth: 1
+      }]
+    };
+
+    this.chart = new Chart("likesChart", {
+      type: "bar" as ChartType,
+      data: data,
+      options: {
+        indexAxis: 'y',
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+    });
 
     this.store.select('posts').subscribe((posts) => {
       this.posts = posts.posts;
@@ -27,11 +63,11 @@ export class DashboardComponent implements OnInit {
         this.numLikes = this.numLikes + post.num_likes;
         this.numDislikes = this.numDislikes + post.num_dislikes;
       });
-    });
-  }
 
-  ngOnInit(): void {
-    this.loadPosts();
+      this.chart.data.datasets[0].data = [this.numLikes, this.numDislikes];
+      this.chart.update();
+    });
+
   }
 
   private loadPosts(): void {
